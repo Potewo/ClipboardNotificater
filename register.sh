71637004face0b13628f6f4af372accf9cb3d27d
Potewo <potewo@protonmail.com>
2021-02-04 13:45:47 +0900

    fix: register daemon in user mode(systemdtl) not system mode

--- register.sh
+++ register.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -ue
 if [ $# != 1 ]
 then
@@ -6,9 +6,13 @@ then
   exit 1
 fi
 
-installpath = $1
+installpath="$1"
+echo $installpath
 cp ./clipboardnotificator.py ${installpath}/clipboardnotificator
-# cp ./clipboardnotificator.service /etc/systemd/system/clipboardnotificator.service
-cat ./clipboardnotificator.service | sed "s%command_path%${installpath}/clipboardnotificator" > /etc/systemd/system/clipboardnotificator.service
-sudo systemctl daemon-reload
-sudo systemctl enable clipboardnotificator
+mkdir -p ~/.config/systemd/user
+touch ~/.config/systemd/user/clipboardnotificator.service
+# sudo cp ./clipboardnotificator.service $HOME/.config/systemd/user/clipboardnotificator.service
+# sudo cat ./clipboardnotificator.service | sed "s%command_path%${installpath}/clipboardnotificator" > /etc/systemd/system/clipboardnotificator.service
+sudo sed -i -e "s%command_path%${installpath}/clipboardnotificator%" ~/.config/systemd/user/clipboardnotificator.service
+systemctl --user daemon-reload
+systemctl --user enable clipboardnotificator
