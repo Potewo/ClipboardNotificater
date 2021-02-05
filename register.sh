d46b7543be9509b71c5f7d6371c12c7c391e2f51
Potewo <potewo@protonmail.com>
2021-02-05 17:56:42 +0900

    Fix: fixed a register.sh bug

--- register.sh
+++ register.sh
@@ -10,8 +10,7 @@ installpath="$1"
 echo $installpath
 cp ./clipboardnotificator.py ${installpath}/clipboardnotificator
 mkdir -p ~/.config/systemd/user
-touch ~/.config/systemd/user/clipboardnotificator.service
-# sudo cp ./clipboardnotificator.service $HOME/.config/systemd/user/clipboardnotificator.service
+cp ./clipboardnotificator.service $HOME/.config/systemd/user/clipboardnotificator.service
 # sudo cat ./clipboardnotificator.service | sed "s%command_path%${installpath}/clipboardnotificator" > /etc/systemd/system/clipboardnotificator.service
 sudo sed -i -e "s%command_path%${installpath}/clipboardnotificator%" ~/.config/systemd/user/clipboardnotificator.service
 systemctl --user daemon-reload
